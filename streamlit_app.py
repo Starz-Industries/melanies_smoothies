9b42432b9a2968fc0eff9549c06465c0cd5cc256
Starz-Industries <stephanrybarz@gmail.com>
2026-02-09 16:29:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,8 @@ st.write('The name on your Smoothie will be: ' + name_on_order)
 
 # FRUITS CONTENT
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
-
 # Convert Snopark dataframe to pandas dataframe
 pd_df = my_dataframe.to_pandas()
-# st.dataframe(data=pd_df)
-# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients'
